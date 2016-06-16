0e3ca7f84bea0fa8241650c1546e989cc72f0feb
baua.gonzo <baua.gonzo@gmail.com>
2016-06-16 15:51:03 +0200

    Add options to :
    * display new announces only
    * configurable cache

--- careers-proservia.rb
+++ careers-proservia.rb
@@ -3,42 +3,65 @@ require 'net/http'
 require 'uri'
 require 'nokogiri'
 require 'yaml'
+require 'optparse'
 require 'awesome_print'
 
-cache = File.dirname(__FILE__) + "/.#{File.basename($0,'.rb')}.jobs.cache"
+options = {}
+op = OptionParser.new do |opts|
+  opts.banner = "Usage: careers-proservia.rb [options]"
+  options[:print_all] = false
+  opts.on("-c file", "--cache file", "Cache file location") do |c|
+    options[:cache] = c
+  end
+  opts.on("-a", "--[no-]all", "Display all announces") do |a|
+    options[:print_all] = a
+  end
+end
+
+begin op.parse! ARGV
+rescue OptionParser::ParseError => e
+  puts e
+  puts op
+  exit 1
+end
+
+@cache = options[:cache].nil? ? File.dirname(__FILE__) + "/.#{File.basename($0,'.rb')}.jobs.cache" : options[:cache]
 begin
-  jobs = YAML::load(File.open(cache))
+  jobs = YAML::load(File.open(@cache))
   jobs = [] unless jobs.is_a?(Array)   # Be sure file is not corrupted
 rescue Errno::ENOENT
   jobs = []
 end
 
-url = "https://recrutement.proservia.fr/public/index.php"
-uri = URI.parse("#{url}?a=annonce")
+@url = "https://recrutement.proservia.fr/public/index.php"
+uri = URI.parse("#{@url}?a=annonce")
 request = Net::HTTP::Post.new(uri)
 request.set_form_data(
-    "a" => "annonce",
-    "localisation[]" => "11570431072",
+  "a" => "annonce",
+  "localisation[]" => "11570431072",
 )
 
 response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == "https") do |http|
-    http.request(request)
+  http.request(request)
 end
 
 html_doc = Nokogiri::HTML(response.body)
 
 html_doc.css(".listcv [class*='row']").each do |j|
-  #ap j['onclick'].values
   x = j.css('td')
-  job = { date: x[0].text, 
+  job = { date: x[0].text,
           title: x[1].text,
           location: x[2].text,
-          link: "#{url + j['onclick'][/(a=.*\d)/]}"
-        }
-  unless jobs.include?(job)
+          link: "#{@url + j['onclick'][/(a=.*\d)/]}"
+  }
+
+  if not jobs.include?(job)
     print "🌟"
+    ap job
     jobs << job
+  elsif options[:print_all]
+    ap job
   end
-  ap job
+
 end
-File.open(cache, 'w') {|f| f.write(YAML.dump(jobs)) }
+File.open(@cache, 'w') { |f| f.write(YAML.dump(jobs)) }
