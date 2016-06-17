a480004e500fa580507ca34cd5109abf7e46f370
baua.gonzo <baua.gonzo@gmail.com>
2016-06-17 15:13:42 +0200

    Apply Ruby style guidelines + fix the missing -i option

--- careers-ovh.rb
+++ careers-ovh.rb
@@ -10,13 +10,16 @@ require 'pry'
 
 options = {}
 op = OptionParser.new do |opts|
-  opts.banner = "Usage: careers-proservia.rb [options]"
+  opts.banner = 'Usage: careers-ovh.rb [options]'
   options[:print_all] = false
-  opts.on("-c file", "--cache file", "Cache file location") do |c|
+  opts.on('-c file', '--cache file', 'Cache file location') do |c|
     options[:cache] = c
   end
-  opts.on("-a", "--[no-]all", "Display all announces") do |a|
+  opts.on('-a', '--[no-]all', 'Display all announces') do |a|
     options[:print_all] = a
+  end
+  opts.on('-i', '--[no-]interactive', 'Launch interactive session') do |i|
+    options[:interactive] = i
   end
 end
 
@@ -27,11 +30,11 @@ rescue OptionParser::ParseError => e
   exit 1
 end
 
-@cache = options[:cache].nil? ? File.dirname(__FILE__) + "/.#{File.basename($0,'.rb')}.jobs.cache" : options[:cache]
+@cache = options[:cache].nil? ? File.dirname(__FILE__) + "/.#{File.basename($0, '.rb')}.jobs.cache" : options[:cache]
 
 begin
-  @jobs = YAML::load(File.open(@cache))
-  @jobs = [] unless @jobs.is_a?(Array)   # Be sure file is not corrupted
+  @jobs = YAML.load(File.open(@cache))
+  @jobs = [] unless @jobs.is_a?(Array) # Be sure file is not corrupted
   @jobs.select { |j| j[:new] }.each { |x| x.delete(:new) }
 rescue Errno::ENOENT
   @jobs = []
@@ -39,14 +42,14 @@ end
 
 @url = 'https://www.ovh.com/fr/careers/offres/resultats.xml?region=Nord-Pas+de+Calais'
 
-['DC', 'DEV_LOGICIEL', 'PROJECT', 'SYSTEMES_RESEAUX', 'SC'].each do |t|
+%w(DC DEV_LOGICIEL PROJECT SYSTEMES_RESEAUX SC).each do |t|
   uri = URI.parse("#{@url}&type=#{t}")
   request = Net::HTTP::Get.new(uri)
   response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == "https") do |http|
     http.request(request)
   end
   html_doc = Nokogiri::HTML(response.body)
-  html_doc.css(".overflow a").each do |j|
+  html_doc.css('.overflow a').each do |j|
     job = {
       category: t,
       title: j.text,
@@ -63,6 +66,6 @@ else
   ap @jobs.select { |x| x[:new] }
 end
 
-File.open(@cache, 'w') {|f| f.write(YAML.dump(@jobs)) }
+File.open(@cache, 'w') { |f| f.write(YAML.dump(@jobs)) }
 
 Pry.start if options[:interactive]
