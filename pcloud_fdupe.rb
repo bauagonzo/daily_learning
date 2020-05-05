098b628ada8057d618e2b6992402f68a1d6131bc
bauagonzo <baua.gonzo@gmail.com>
2020-05-05 15:51:11 -0400

    Find pcloud duplicate files

---
+++ pcloud_fdupe.rb
@@ -0,0 +1,34 @@
+require 'httparty'
+require 'json'
+
+ENV['PCLOUD_USER'] ||= 'baua.gonzo@gmail.com'
+ENV['PCLOUD_PASSWORD'] ||= ''
+ENV['PCLOUD_DRYRUN'] ||= 'true'
+class Pcloud
+  include HTTParty
+  base_uri 'https://api.pcloud.com/'
+end
+
+# something nil or empty means dir so skip it
+def get_all_files(dir, path = '/', acc = Hash.new {|h,k| h[k] = [] })
+   dir['contents'].each do |x|
+    get_all_files(x,"#{path}/#{x['name']}", acc) unless x['contents'].nil? || x['contents'].empty?
+    acc[x['hash']] << x unless x['hash'].nil?
+  end
+  acc
+end
+
+user = Pcloud.get('/userinfo', query: {getauth: 1,username: ENV['PCLOUD_USER'], password: ENV['PCLOUD_PASSWORD']})
+Pcloud.cookies(auth: user['auth'])
+res = JSON.parse(Pcloud.get('/listfolder', query: {path: '/MyMusic', recursive: 1}).response.body)
+
+all_files = get_all_files(res['metadata'], res['metadata']['path'])
+
+all_files.select {|k, v| v.size > 1}.each do |k,v|
+  # keep the first file in the list
+  # TODO give an option to select which file to keep or drop
+  v.drop(1).each do |f|
+    puts Pcloud.get('/stat', query: {fileid: f['fileid']})
+    Pcloud.delete('/delete', query: {fileid: f['fileid']}) if ENV['PCLOUD_DRYRUN'] == 'false'
+  end
+end
